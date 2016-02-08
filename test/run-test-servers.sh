956334052456c19c2bf8cd79536d08f7b154f4d9
camros <jam@camros.com>
2016-02-08 14:33:00 -0500

    Fixup starting

--- test/run-test-servers.sh
+++ test/run-test-servers.sh
@@ -6,18 +6,39 @@
 # for our integration tests. Trying to controll all this in elixir vs.
 # the shell (for now) is a hack.
 #
-# This is expected to be run from the project's root directory
+# This is expected to be run from the project's root directory. See notes on
+# ports (below).
+#
+# FIXME: jam: test on windows and other platforms (or adapt?)
+#
+
+#set -xv
 
 ME="`basename $0`"
-GNATSD=gnatsd
+DEFAULT_GNATSD=gnatsd
+#DEFAULT_GNATSD=go run gnatsd.go
+GNATSD=${*:-$DEFAULT_GNATSD}
 
-
-run_gnats() {
-  echo "$ME: starting NATS server with config: $1"
-  "$GNATSD" -c "./test/conf/$1" < /dev/null > /dev/null 2>&1 &
+check_gnatsd() {
+  echo "$ME: unable to find GNATSD executable: $1" >&2
+  echo "$ME: ensure its in your PATH or pass it explicitly to this script" >&2
+  exit 1
 }
 
-# default server... no config
-run_gnats plain.conf
-run_gnats auth.conf
-run_gnats tls.conf
+run_gnats() {
+    echo "$ME: starting NATS server: $@"
+    $@ < /dev/null 2>&1 &
+
+#check_gnatsd
+    
+}
+
+echo "$ME: starting NATS servers... (hit ^C or kill this job to stop servers)"
+# NOTES on servers and ports for tests...
+# these are all set to run on specific ports till someone gets around
+# to automating all the tests to start/stop/kill! servers on specific ports
+# within elixir. till then this script is a wrapper :-(
+run_gnats $GNATSD -c test/conf/plain.conf
+run_gnats $GNATSD -c test/conf/auth.conf
+run_gnats $GNATSD -c test/conf/tls.conf
+wait
