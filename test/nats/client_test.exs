9b1a138447bb82d072a38d3213f9ec26b9fd8f82
eaglens <magnus.elenstal@ericsson.com>
2017-01-30 14:52:59 +0100

    Added possibility to register a name for the Nats.Client

--- test/nats/client_test.exs
+++ test/nats/client_test.exs
@@ -46,6 +46,18 @@ defmodule Nats.ClientTest do
     :ok = Client.flush(con)
   end
 
+  test "Open a named client" do
+    subject = "FOO-bar"
+
+    {:ok, _ } = Client.start_link :test_client
+    {:ok, ref1} = Client.sub(:test_client, self(), subject)
+    :ok == Client.unsub(:test_client, ref1)
+
+    {:ok, _ } = Client.start :test_client2
+    {:ok, ref2} = Client.sub(:test_client2, self(), subject)
+    :ok == Client.unsub(:test_client2, ref2)
+  end
+
   test "Client wants tls vs. server doesn't" do
     opts = %{ tls_required: true, port: TestHelper.default_port, }
     {:error, _why } = Client.start opts
